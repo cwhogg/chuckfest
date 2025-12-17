08bf624dfdfa1217d9720de61467d0cdb0ecf768
cwhogg <cwhogg@gmail.com>
2025-12-16 20:40:48 -0800

    Zoom out trip maps to show California-wide context

--- src/components/trip-location-map.tsx
+++ src/components/trip-location-map.tsx
@@ -85,7 +85,7 @@ export function TripLocationMap({ latitude, longitude, className = '' }: TripLoc
     <div className={`trip-location-map ${className}`}>
       <MapContainer
         center={position}
-        zoom={10}
+        zoom={5}
         style={{ width: '100%', height: '100%' }}
         scrollWheelZoom={false}
         dragging={false}
